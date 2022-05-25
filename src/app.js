f97cbc2b18c2e2f6e192e0e37739403c6e0c0be4
Feerny <luisfpe19@gmail.com>
2022-05-25 11:30:13 -0500

    termine pag

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const URL = "https://random.dog/woof.json"
+
+const button = document.getElementById("button");
+const contenedor = document.getElementById("contenedor");
+
+
+const fetchData = async(api)=>{
+    const res = await fetch(api);
+    const data = await res.json();
+    return data;
+}
+
+
+const getCharacter = async()=> {
+const data = await fetchData(URL);
+
+console.log(data);
+const carta = document.createElement('div');
+const imagen = document.createElement('img');
+const video = document.createElement('video');
+
+if (/.(mp4)$/i.test(data.url)) {
+    video.setAttribute('src',data.url);
+    video.setAttribute('alt',data.fileSizeBytes);
+    video.setAttribute('Autoplay',true)
+    video.setAttribute('Loop',true)
+    carta.appendChild(video);
+    contenedor.appendChild(carta);
+}
+else if (/.(jpg)$/i.test(data.url)) {
+    imagen.setAttribute('src',data.url);
+    imagen.setAttribute('alt',data.fileSizeBytes);
+    carta.appendChild(imagen);
+    contenedor.appendChild(carta);
+}
+
+carta.setAttribute('class',"cartas");
+
+}
+
+button.addEventListener('click',getCharacter);
